491213069553761cdfdb7bfaa3c7e608ff9c645d
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-28 19:56:12 -0400

    mvp

--- Venting/src/util.js
+++ Venting/src/util.js
@@ -1,18 +1,10 @@
 import React from 'react';
 
-const EMOTIONS = {
-  'angry': 'angry',
-  'sad': 'sad',
-  'hungry': 'hungry',
-  'tired': 'tired',
-  'sleepy': 'sleepy',
-  'happy': 'happy',
-  'excited': 'excited',
-};
+const EMOTIONS = ['angry', 'sad', 'hungry', 'tired', 'sleepy', 'happy', 'excited'];
 
 const emotionize = sentence => {
   let s = sentence.split(':');
-  let mood = s.length == 1 ? '' : s[0];
+  let mood = s.length == 1 ? '' : s[0].toLowerCase();
   let literalSentence = s.length == 1 ? s[0] : s[1];
 
   return (
